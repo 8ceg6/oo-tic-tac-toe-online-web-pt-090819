347adc48fe268669fc223c7715f2fcf4c9c344b5
cedric e goddard <8ceg6@comcast.net>
2019-10-12 13:03:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,4 +48,9 @@ class TicTacToe
      def current_player
           turn_count.even? ? "X" : "O"
      end
+     
+     def turn 
+       puts "Choose a number between 1-9"
+       input = gets.chomp
+       end 
 end
